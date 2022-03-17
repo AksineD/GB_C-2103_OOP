61ea377ccb2b6524af65c3019fb53d200524f573
Denis Y <denis.y.v@gmail.com>
2022-03-17 14:22:48 +0200

    task 1

---
+++ BankAPP/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAPP
+{
+    using Newtonsoft.Json;
+
+    enum BankAccountType
+    {
+        Current,
+        Savings,
+        Foreign
+
+    }
+
+    internal class BankAccount
+    {
+        private int _accountNumber;
+
+        private double _balance;
+
+        private BankAccountType _accountType;
+
+        public void SetAccountNumber(int accNum)
+        {
+            _accountNumber = accNum;
+        }
+
+        public int GetAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        public void SetBalance(double balance)
+        {
+            _balance = balance;
+        }
+
+        public double GetBalance()
+        {
+            return this._balance;
+        }
+
+        public void SetAccountType(BankAccountType type)
+        {
+            _accountType = type;
+        }
+
+        public BankAccountType GetAccountType()
+        {
+            return _accountType;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Account Number: {this.GetAccountNumber()} ; ");
+            sb.Append($"Balance: {this.GetBalance()} ; ");
+            sb.Append($"Account Type: {this.GetAccountType()} ; ");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ BankAPP/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using BankAPP;
+
+var ba = new BankAccount();
+
+ba.SetAccountNumber(1234);
+ba.SetBalance(-2356.45);
+ba.SetAccountType(BankAccountType.Savings);
+
+
+
+Console.WriteLine(ba.ToString());
